2f414d0432e5b871c8d124afda266fb65efbe641
WQG6848 <863844516@qq.com>
2024-03-05 23:35:59 +0800

    feat: update

---
+++ dep_third.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+programExists() {
+    local ret='0'
+    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # fail on non-zero return value
+    if [ "$ret" -ne 0 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+programExists ffmpeg
+if [ $? -ne 0 ]; then
+    VERSION=ffmpeg-6.0-arm64-static
+    echo "install ${VERSION} ..."
+    
+    wget https://www.johnvansickle.com/ffmpeg/old-releases/${VERSION}.tar.xz
+    tar -xzf ${VERSION}.tar.xz
+    sudp cp ${VERSION}/ff* /usr/bin/
+fi
---
+++ packet.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ROOT=$(dirname "$PWD")
+
+rm -rf output || true
+mkdir output
+
+build() {
+    project=${ROOT}/$1
+    branch=$2
+    echo "start to build project [${project}] with branch [${branch}]"
+    cd ${project}
+    git checkout ${branch}
+    ./build.sh
+    mv ./output ${ROOT}/installer/output/$1
+    cd -
+}
+
+build algo-manager-service feat/v2
+build client feat/v2
+build config-manager-service main
+build deploy main
+build image-manager-service feat/v2
+build manage-core-service main
+build open-gateway main
+build system-manager-service main
+build video-ingress-service feat/v2
+build video-process-service feat/v2
+
+SUFFIX_GIT=$(git rev-parse --short HEAD)
+SUFFIX_DATE=$(date "+%Y%m%d")
+cd output 
+tar -zcvf ./infuturetech-${SUFFIX_GIT}-${SUFFIX_DATE}.tar.gz ./
