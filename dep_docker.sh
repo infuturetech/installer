a1ffb9542167fa2a0f241962974f9fc2e08445e7
WQG6848 <863844516@qq.com>
2024-08-13 22:26:12 +0800

    feat: update

--- dep_docker.sh
+++ dep_docker.sh
@@ -4,3 +4,6 @@ sudo docker pull infuturetech/algo:v1.0.1
 sudo docker pull mysql
 sudo docker pull minio/minio
 sudo docker pull nsqio/nsq
+sudo docker pull prom/prometheus:latest
+sudo docker pull infuturetech/jetson_stats_node_exporter:v1.0.0
+sudo docker pull grafana/grafana-enterprise:latest
