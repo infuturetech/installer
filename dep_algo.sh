9e4e67839edf614dfec93e4e48bd02975a8445bc
WQG6848 <863844516@qq.com>
2024-08-21 22:47:13 +0800

    feat: update

--- dep_algo.sh
+++ dep_algo.sh
@@ -1,7 +1,11 @@
 NAME=wrapper-20240618-3f55fd9.tar.gz
-wget https://github.com/infuturetech/installer/releases/download/debug/${NAME}
-tar -xzf ${NAME}
+if [ ! -f $NAME ];then
+  wget https://github.com/infuturetech/installer/releases/download/debug/${NAME}
+else
+    tar -xzf ${NAME}
+fi
 
 echo "install wrapper to /opt/infuturetech/"
 rm -rf /opt/infuturetech/wrapper || true
+mkdir -p /opt/infuturetech/
 mv wrapper /opt/infuturetech/
